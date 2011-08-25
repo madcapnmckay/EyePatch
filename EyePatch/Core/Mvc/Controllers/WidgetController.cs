5afd23060dd18d2f87df6d1ce99e474a010befcb
Ian Mckay <imm102@hotmail.com>
2011-08-24 23:12:09 -0700

    Bug fix for html widget add onto an empty canvas.

--- EyePatch/Core/Mvc/Controllers/WidgetController.cs
+++ EyePatch/Core/Mvc/Controllers/WidgetController.cs
@@ -6,44 +6,45 @@ namespace EyePatch.Core.Mvc.Controllers
     [Authorize(Roles = "Admin")]
     public class WidgetController : BaseController
     {
-        public WidgetController(IContentManager contentManager) : base(contentManager)
+        public WidgetController(IContentManager contentManager)
+            : base(contentManager)
         {
         }
 
         [HttpPost]
         public JsonResult Add(string pageId, string widgetId, string contentAreaId, int position, string sourceUrl)
         {
+            var pageJs = contentManager.Page.Js(pageId);
+            var pageCss = contentManager.Page.Css(pageId);
+
             return
                 JsonNet(
                     new
                         {
                             success = true,
                             widget =
-                        contentManager.Widget.Add(pageId, widgetId, contentAreaId, position).ToViewModel(pageId,
-                                                                                                         contentManager,
-                                                                                                         this, sourceUrl)
-                        });
+                        contentManager.Widget.Add(pageId, widgetId, contentAreaId, position).ToViewModel(pageJs, pageCss, contentManager, this, sourceUrl) });
         }
 
         [HttpPost]
         public JsonResult Remove(string pageId, string widgetId)
         {
             contentManager.Widget.Delete(pageId, widgetId);
-            return JsonNet(new {success = true});
+            return JsonNet(new { success = true });
         }
 
         [HttpPost]
         public JsonResult Move(string pageId, string widgetId, string contentAreaId, int position)
         {
             contentManager.Widget.Move(pageId, widgetId, contentAreaId, position);
-            return JsonNet(new {success = true});
+            return JsonNet(new { success = true });
         }
 
         [HttpPost]
         public JsonResult Sort(string pageId, string widgetId, int position)
         {
             contentManager.Widget.Sort(pageId, widgetId, position);
-            return JsonNet(new {success = true});
+            return JsonNet(new { success = true });
         }
     }
 }
