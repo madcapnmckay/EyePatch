ed2114c2705083d9a09d9926631ff54234749e24
Ian Mckay <imm102@hotmail.com>
2011-08-30 18:59:25 -0700

    Helper methods for returning friendly urls + slugs.

--- EyePatch/Core/Util/Url.cs
+++ EyePatch/Core/Util/Url.cs
@@ -1,4 +1,7 @@
-﻿using System.Web;
+﻿using System;
+using System.Collections.Specialized;
+using System.Text;
+using System.Web;
 
 namespace EyePatch.Core.Util
 {
@@ -15,6 +18,191 @@ namespace EyePatch.Core.Util
 
             var test = VirtualPathUtility.Combine(resolvedBase, relativeUrl);
             return test;
+        }
+
+        public static string WithQueryString(this string path, NameValueCollection query)
+        {
+            return path + "?" + string.Join("&", Array.ConvertAll(query.AllKeys, key => string.Format("{0}={1}", HttpUtility.UrlEncode(key), HttpUtility.UrlEncode(query[key]))));
+        }
+
+        public static string AsFriendly(this string url)
+        {
+            if (url == null) return "";
+
+            const int maxlen = 80;
+            int len = url.Length;
+            bool prevdash = false;
+            var sb = new StringBuilder(len);
+            char c;
+
+            for (int i = 0; i < len; i++)
+            {
+                c = url[i];
+                if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9'))
+                {
+                    sb.Append(c);
+                    prevdash = false;
+                }
+                else if (c >= 'A' && c <= 'Z')
+                {
+                    // tricky way to convert to lowercase
+                    sb.Append((char)(c | 32));
+                    prevdash = false;
+                }
+                else if (c == ' ' || c == ',' || c == '.' || c == '/' || c == '\\' || c == '-' || c == '_' || c == '=')
+                {
+                    if (!prevdash && sb.Length > 0)
+                    {
+                        sb.Append('-');
+                        prevdash = true;
+                    }
+                }
+                else if ((int)c >= 128)
+                {
+                    int prevlen = sb.Length;
+                    sb.Append(RemapInternationalCharToAscii(c));
+                    if (prevlen != sb.Length) prevdash = false;
+                }
+                if (i == maxlen) break;
+            }
+
+            if (prevdash)
+                return sb.ToString().Substring(0, sb.Length - 1);
+            else
+                return sb.ToString();
+        }
+
+
+        public static string AsSlug(this string url)
+        {
+            if (url == null) return "";
+
+            const int maxlen = 80;
+            int len = url.Length;
+            bool prevdash = false;
+            var sb = new StringBuilder(len);
+            char c;
+
+            for (int i = 0; i < len; i++)
+            {
+                c = url[i];
+                if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9'))
+                {
+                    sb.Append(c);
+                    prevdash = false;
+                }
+                else if (c >= 'A' && c <= 'Z')
+                {
+                    // tricky way to convert to lowercase
+                    sb.Append((char)(c | 32));
+                    prevdash = false;
+                }
+                else if (c == ' ' || c == ',' || c == '.' || c == '/' || c == '\\' || c == '-' || c == '_' || c == '=')
+                {
+                    if (!prevdash && sb.Length > 0)
+                    {
+                        sb.Append('-');
+                        prevdash = true;
+                    }
+                }
+                else if ((int)c >= 128)
+                {
+                    int prevlen = sb.Length;
+                    sb.Append(RemapInternationalCharToAscii(c));
+                    if (prevlen != sb.Length) prevdash = false;
+                }
+                else
+                {
+                    sb.Append(HttpUtility.UrlEncode(c.ToString()));
+                }
+                if (i == maxlen) break;
+            }
+
+            if (prevdash)
+                return sb.ToString().Substring(0, sb.Length - 1);
+            else
+                return sb.ToString();
+        }
+
+        public static string RemapInternationalCharToAscii(char c)
+        {
+            string s = c.ToString().ToLowerInvariant();
+            if ("àåáâäãåą".Contains(s))
+            {
+                return "a";
+            }
+            else if ("èéêëę".Contains(s))
+            {
+                return "e";
+            }
+            else if ("ìíîïı".Contains(s))
+            {
+                return "i";
+            }
+            else if ("òóôõöøőð".Contains(s))
+            {
+                return "o";
+            }
+            else if ("ùúûüŭů".Contains(s))
+            {
+                return "u";
+            }
+            else if ("çćčĉ".Contains(s))
+            {
+                return "c";
+            }
+            else if ("żźž".Contains(s))
+            {
+                return "z";
+            }
+            else if ("śşšŝ".Contains(s))
+            {
+                return "s";
+            }
+            else if ("ñń".Contains(s))
+            {
+                return "n";
+            }
+            else if ("ýÿ".Contains(s))
+            {
+                return "y";
+            }
+            else if ("ğĝ".Contains(s))
+            {
+                return "g";
+            }
+            else if (c == 'ř')
+            {
+                return "r";
+            }
+            else if (c == 'ł')
+            {
+                return "l";
+            }
+            else if (c == 'đ')
+            {
+                return "d";
+            }
+            else if (c == 'ß')
+            {
+                return "ss";
+            }
+            else if (c == 'Þ')
+            {
+                return "th";
+            }
+            else if (c == 'ĥ')
+            {
+                return "h";
+            }
+            else if (c == 'ĵ')
+            {
+                return "j";
+            }
+            else
+            {
+                return "";
+            }
         }
     }
 }
