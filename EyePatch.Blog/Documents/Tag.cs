3e49e977a53bbd0cf5ec27b081b67864c8ebc33e
Ian Mckay <imm102@hotmail.com>
2011-08-30 18:56:20 -0700

    Tag now contains a slug value to be used in routes.

--- EyePatch.Blog/Documents/Tag.cs
+++ EyePatch.Blog/Documents/Tag.cs
@@ -1,7 +1,21 @@
-﻿namespace EyePatch.Blog.Documents
+﻿using EyePatch.Core.Util;
+
+namespace EyePatch.Blog.Documents
 {
     public class Tag
     {
         public string Value { get; set; }
+        public string Slug { get; set; }
+
+        public Tag()
+        {
+            
+        }
+
+        public Tag(string tag)
+        {
+            Value = tag;
+            Slug = tag.AsSlug();
+        }
     }
 }
