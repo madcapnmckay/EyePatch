95d6a24f0e8234c26fc1c596893ac36512e9e1ab
Ian Mckay <imm102@hotmail.com>
2011-08-30 19:08:18 -0700

    Added.

---
+++ EyePatch.Blog/Models/Widgets/PostList.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Web;
+using EyePatch.Core.Util;
+
+namespace EyePatch.Blog.Models.Widgets
+{
+    public class PostList : List<PostSummary>
+    {
+        protected int currentPage;
+        protected int pageSize;
+        protected int totalRecords;
+
+        public bool PagerVisible
+        {
+            get { return totalRecords > pageSize; }
+        }
+
+        public bool OlderVisible
+        {
+            get
+            {
+                var last = Math.Ceiling(totalRecords / (double)pageSize);
+                return currentPage < last;
+            }
+        }
+
+        public bool NewerVisible
+        {
+            get
+            {
+                return currentPage > 1;
+            }
+        }
+
+        public string OlderLink
+        {
+            get
+            {
+                if (!OlderVisible)
+                    return string.Empty;
+
+                var query = new NameValueCollection(HttpContext.Current.Request.QueryString);
+                var newPage = (currentPage + 1).ToString();
+
+                if (query["page"] == null)
+                    query.Add("page", newPage);
+                else
+                    query["page"] = newPage;
+
+                return HttpContext.Current.Request.Path.WithQueryString(query); 
+            }
+        }
+
+        public string NewerLink
+        {
+            get {
+                if (!NewerVisible)
+                    return string.Empty;
+
+                var query = new NameValueCollection(HttpContext.Current.Request.QueryString);
+                var newPage = (currentPage - 1).ToString();
+                
+                if (query["page"] == null)
+                    query.Add("page", newPage);
+                else
+                    query["page"] = newPage;
+
+                return HttpContext.Current.Request.Path.WithQueryString(query); 
+            }
+        }
+
+        public PostList(int currentPage, int pageSize, int totalRecords, IEnumerable<PostSummary> posts)
+        {
+            this.currentPage = currentPage;
+            this.pageSize = pageSize;
+            this.totalRecords = totalRecords;
+            this.AddRange(posts);
+        }
+    }
+}
---
+++ EyePatch.Blog/Models/Widgets/TagCloud.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EyePatch.Blog.Models.Widgets
+{
+    public class TagCloud : List<TagCloudItem>
+    {
+        protected int totalPosts;
+
+        public TagCloud(IEnumerable<TagCloudItem> items, int totalPosts)
+        {
+            this.totalPosts = totalPosts;
+            AddRange(items);
+        }
+
+        public string CssClass(TagCloudItem tag)
+        {
+            var result = (tag.Count / (double)totalPosts);
+            return string.Format("tag{0}", Math.Ceiling(result / 0.05));
+        }
+    }
+}
