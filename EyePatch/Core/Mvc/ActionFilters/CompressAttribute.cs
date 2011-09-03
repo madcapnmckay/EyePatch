165305816a36cc51b76a0e3acf0303ab52ba3821
Ian Mckay <imm102@hotmail.com>
2011-09-02 19:48:58 -0700

    Remove base call.

--- EyePatch/Core/Mvc/ActionFilters/CompressAttribute.cs
+++ EyePatch/Core/Mvc/ActionFilters/CompressAttribute.cs
@@ -1,10 +1,13 @@
 ﻿using System.IO.Compression;
+using System.Text.RegularExpressions;
 using System.Web.Mvc;
 
 namespace EyePatch.Core.Mvc.ActionFilters
 {
     public class CompressAttribute : ActionFilterAttribute
     {
+        protected static Regex adminPanelRegex = new Regex("</head>", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
             //get request and response
@@ -29,8 +32,6 @@ namespace EyePatch.Core.Mvc.ActionFilters
                 response.AppendHeader("Content-encoding", "gzip");
                 response.Filter = new GZipStream(response.Filter, CompressionMode.Compress);
             }
-
-            base.OnActionExecuting(filterContext);
         }
     }
 }
