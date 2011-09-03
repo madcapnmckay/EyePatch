755ac9bfda21705ffd14715bb202f35b74654e89
Ian Mckay <imm102@hotmail.com>
2011-09-02 19:48:50 -0700

    Merge compress and google analytics filters.

--- EyePatch/Core/Mvc/ActionFilters/GoogleAnalyticsAttribute.cs
+++ EyePatch/Core/Mvc/ActionFilters/GoogleAnalyticsAttribute.cs
@@ -1,24 +1,42 @@
 ﻿using System.Collections.Generic;
+using System.IO.Compression;
 using System.Text.RegularExpressions;
-using System.Web;
 using System.Web.Mvc;
-using System.Web.Routing;
 using EyePatch.Core.Documents;
 using EyePatch.Core.Mvc.Resources;
-using EyePatch.Core.Services;
 using EyePatch.Core.Util.Extensions;
-using StructureMap;
 
 namespace EyePatch.Core.Mvc.ActionFilters
 {
     public class GoogleAnalyticsAttribute : ActionFilterAttribute
     {
         protected static Regex adminPanelRegex = new Regex("</head>", RegexOptions.IgnoreCase | RegexOptions.Compiled);
-        // bad monkey! need to inject
-        protected IResourceService resourceService = ObjectFactory.GetInstance<IResourceService>();
 
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
+            //get request and response
+            var request = filterContext.HttpContext.Request;
+            var response = filterContext.HttpContext.Response;
+
+            //get encoding
+            var encoding = request.Headers["Accept-Encoding"];
+            if (string.IsNullOrWhiteSpace(encoding))
+                return;
+
+            encoding = encoding.ToUpperInvariant();
+
+
+            if (encoding.Contains("DEFLATE") || encoding == "*")
+            {
+                response.AppendHeader("Content-encoding", "deflate");
+                response.Filter = new DeflateStream(response.Filter, CompressionMode.Compress);
+            }
+            else if (encoding.Contains("GZIP"))
+            {
+                response.AppendHeader("Content-encoding", "gzip");
+                response.Filter = new GZipStream(response.Filter, CompressionMode.Compress);
+            }
+
             var urlHelper = new UrlHelper(filterContext.RequestContext);
 
             var page = filterContext.RouteData.Values["epPage"] as Page;
@@ -27,14 +45,14 @@ namespace EyePatch.Core.Mvc.ActionFilters
                 return;
 
             var analytics = new ResourcePath
-                                {
-                                    ContentType = "text/javascript",
-                                    Url = urlHelper.ActionSeo("Analytics", "Content", new { id = page.Id })
-                                };
+            {
+                ContentType = "text/javascript",
+                Url = urlHelper.RouteUrl("Default",  new { action = "Analytics", controller = "Content", id = page.Id }).ToLowerInvariant()
+            };
 
-            var response = filterContext.HttpContext.Response;
+            var tags = new List<ResourcePath> { analytics }.ToTags();
             response.Filter = new RegexResponseFilter(response.Filter, adminPanelRegex,
-                                                      string.Format("{0}</head>", new List<ResourcePath> {analytics}.ToTags()));
+                                                      string.Format("{0}</head>", tags));
         }
     }
 }
