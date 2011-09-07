f2b79a69aafdd3b7438d56b0c12b28cfa336c3ef
Ian Mckay <imm102@hotmail.com>
2011-09-06 18:11:07 -0700

    Blog settings remember the TemplateId.

--- EyePatch.Blog/Documents/Blog.cs
+++ EyePatch.Blog/Documents/Blog.cs
@@ -2,6 +2,7 @@
 {
     public class Blog
     {
+        public string TemplateId { get; set; }
         public string Id { get; set; }
         public string ListPageId { get; set; }
         public string PostPageId { get; set; }
